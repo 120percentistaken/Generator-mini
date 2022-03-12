9f38ee910bc5c3749355fe9f94e7f4809607c256
no <53334539+120percentistaken@users.noreply.github.com>
2022-03-12 14:47:02 +0800

    Added method when button is clicked
    Button shows random user.

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ const app = Vue.createApp({
                 picture:'https://randomuser.me/api/portraits/men/10.jpg',
             }
         },
+        methods: {
+           async getUser() {
+               //get a random user data every time you click
+               const res = await fetch('https://randomuser.me/api')
+               //results array, user by default
+               const {results} = await res.json()
+               
+                this.firstName = results[0].name.first
+                this.lastName = results[0].name.last
+                this.gender = results[0].gender
+                this.email = results[0].email
+                this.picture = results[0].picture.large
+            }, 
+        },
 })
 
 app.mount('#app')
